8435afbbc7e73f18c2d0c01e7425f12e5b6d9690
George Zhu <georgezhu211@gmail.com>
2026-03-02 21:00:18 -0600

    ejs

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
+const path = require("node:path");
 
-app.get("/", (req, res) => res.send("Hello, world!"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => res.render("index"));
+app.get("/new", (req, res) => res.send("new"));
 
 const PORT = 3000;
+
 app.listen(PORT, (error) => {
   // This is important!
   // Without this, any startup errors will silently fail
