f6fba9be13a75b216f0abd044f6524b1b2f37797
George Zhu <georgezhu211@gmail.com>
2026-03-02 21:20:32 -0600

    messages

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const path = require("node:path");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
 const indexRouter = require("./routes/indexRouter");
 
 app.use(indexRouter);
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,9 +1,22 @@
 // routes/indexRouter.js
 const { Router } = require("express");
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 const indexRouter = Router();
 
-indexRouter.get("/", (req, res) => res.render("index"));
+indexRouter.get("/", (req, res) => res.render("index", { messages }));
 indexRouter.get("/new", (req, res) => res.render("new"));
 
 module.exports = indexRouter;
