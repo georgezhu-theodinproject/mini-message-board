a4aae632a442751991ab07367da06425dd07a866
George Zhu <georgezhu211@gmail.com>
2026-03-02 22:02:57 -0600

    detail

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,11 +3,13 @@ const { Router } = require("express");
 
 const messages = [
   {
+    id: crypto.randomUUID(),
     text: "Hi there!",
     user: "Amando",
     added: new Date(),
   },
   {
+    id: crypto.randomUUID(),
     text: "Hello World!",
     user: "Charles",
     added: new Date(),
@@ -17,11 +19,25 @@ const messages = [
 const indexRouter = Router();
 
 indexRouter.get("/", (req, res) => res.render("index", { messages }));
+
+indexRouter.get("/messages/:messageId", (req, res) => {
+  const id = req.params.messageId;
+  res.render("message", {
+    message: messages.find((message) => message.id === id),
+  });
+});
+
 indexRouter.get("/new", (req, res) => res.render("form"));
+
 indexRouter.post("/new", (req, res) => {
   const messageText = req.body.text;
   const messageUser = req.body.user;
-  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  messages.push({
+    id: crypto.randomUUID(),
+    text: messageText,
+    user: messageUser,
+    added: new Date(),
+  });
   res.redirect("/");
 });
 
